884bf430f84d0472cc9a2cde87a926b65244c2f5
Suraj124 <sam124.sy@gmail.com>
2019-04-02 05:26:12 +0530

    start

---
+++ BlackJack.py
@@ -0,0 +1,5 @@
+import random
+
+suit=('Hearts','Spades','Diamonds','Clubs')
+rank=('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','jack','King','Queen','Ace')
+values={'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8,'Nine':9,'Ten':10,'Jack':10,'King':10,'Queen':10,'Ace':11}
