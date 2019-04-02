a8c9311dc44fe2eedc3bffbdb824fbbbf2aa3334
Suraj124 <sam124.sy@gmail.com>
2019-04-02 05:39:38 +0530

    All cards of Deck

--- BlackJack.py
+++ BlackJack.py
@@ -1,5 +1,40 @@
 import random
 
-suit=('Hearts','Spades','Diamonds','Clubs')
-rank=('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','jack','King','Queen','Ace')
+suits=('Hearts','Spades','Diamonds','Clubs')
+ranks=('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','jack','King','Queen','Ace')
 values={'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8,'Nine':9,'Ten':10,'Jack':10,'King':10,'Queen':10,'Ace':11}
+
+class Card():
+
+    def __init__(self,suit,rank):
+        self.suit=suit
+        self.rank=rank
+    
+    def __str__(self):
+        return f'{self.rank} of {self.suit}'
+class Deck():
+
+    def __init__(self):
+        self.deck=[]
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+    
+    def __str__(self):
+        deck_comp=''
+        for card in self.deck:
+            deck_comp+='\n'+card.__str__()
+        return deck_comp
+    
+    def shuffle(self):
+        random.shuffle(self.deck)
+    
+    def deal(self):
+        return self.deck.pop()
+
+    def __len__(self):
+        return len(self.deck)
+
+obj=Deck()
+print(obj)
+print(len(obj))
