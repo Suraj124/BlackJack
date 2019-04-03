8fa44130bd4929865ed44b673f3a4755cb74a0a7
Suraj124 <sam124.sy@gmail.com>
2019-04-03 06:43:05 +0530

    Final

--- BlackJack.py
+++ BlackJack.py
@@ -1,7 +1,7 @@
 import random
 
 suits=('Hearts','Spades','Diamonds','Clubs')
-ranks=('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','jack','King','Queen','Ace')
+ranks=('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','King','Queen','Ace')
 values={'Two':2,'Three':3,'Four':4,'Five':5,'Six':6,'Seven':7,'Eight':8,'Nine':9,'Ten':10,'Jack':10,'King':10,'Queen':10,'Ace':11}
 playing=True
 
@@ -71,7 +71,7 @@ def take_bet(chips):
             print('Sorry, a bet must be a integer')
         else:
             if chips.bet>chips.total:
-                print(f'You don\'t have enough chips. Currently you have {self.total}')
+                print(f'You don\'t have enough chips. Currently you have {chips.total}')
             else:
                 break
 def hit(deck,hand):
@@ -80,10 +80,99 @@ def hit(deck,hand):
     hand.adjust_for_aces()
 
 
-d=Deck()
-d.shuffle()
-pulled_card=d.deal()
-print(pulled_card)
-p1=Hand()
-p1.add_cards(pulled_card)
-print(p1.value)
+
+def hit_or_stand(deck,hand):
+    global playing
+    while True:
+        x=input("Do you wan to hit or stand ? h or s : ")
+
+        if x[0].lower()=='h':
+            hit(deck,hand)
+        elif x[0].lower()=='s':
+            print("Player stand and now dealer turn")
+            playing=False
+        else:
+            print("I dont understand, Please enter h or s only!!!")
+            continue
+        break
+
+def show_some(player,dealer):
+    print("Dealer's card")
+    print("<One card Hidden>")
+    print(dealer.cards[1])
+    print(" \n")
+    print("Player Cards",*player.cards,sep="\n")
+def show_all(player,dealer):
+    print("\nDealer's card",*dealer.cards,sep='\n')
+    print("\nDealer's value",dealer.value,sep='\n')
+    print("\nPlayer's card",*player.cards,sep='\n')
+    print("\nPlayer's value",player.value,sep='\n')
+def player_busts(chips):
+    print("Player BUSTS")
+    chips.lose_bet()
+def player_win(chips):
+    print("Player WIN")
+    chips.win_bet()
+def dealer_busts(chips):
+    print("Delear BUSTS")
+    chips.win_bet()
+def delear_win(chips):
+    print("Player BUSTS")
+    chips.lose_bet()
+def push():
+    print("Player and Delaer Tie")
+
+#Game Start
+
+while True:
+    deck=Deck()
+    deck.shuffle()
+
+    player_hand=Hand()
+    player_hand.add_cards(deck.deal())
+    player_hand.add_cards(deck.deal())
+
+    dealer_hand=Hand()
+    dealer_hand.add_cards(deck.deal())
+    dealer_hand.add_cards(deck.deal())
+
+    player_chips=Chips()
+    take_bet(player_chips)
+
+    show_some(player_hand,dealer_hand)
+
+    while playing:
+
+        hit_or_stand(deck,player_hand)
+
+        show_some(player_hand,dealer_hand)
+
+        if player_hand.value>21:
+            player_busts(player_chips)
+            break
+    if player_hand.value<21:
+
+        while dealer_hand.value<player_hand.value:
+            hit(deck,dealer_hand)
+        
+        show_all(player_hand,dealer_hand)
+        
+        if dealer_hand.value>21:
+            dealer_busts(player_chips)
+        elif dealer_hand.value<player_hand.value:
+            dealer_busts(player_chips)
+        elif dealer_hand.value>player_hand.value:
+            delear_win(player_chips)
+        else:
+            push()
+    print(f"Player currently have {player_chips.total}")
+
+    new_game=input("Do you want to play agaian? y/n ? : ")
+    if new_game[0].lower()=='y':
+        playing=True
+        continue
+    else:
+        break
+
+
+
